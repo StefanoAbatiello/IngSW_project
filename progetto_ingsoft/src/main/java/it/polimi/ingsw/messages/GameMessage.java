5d8538aaa866f30c9bc7a770a251b798dbd0e503
StefanoAbatiello <80035914+StefanoAbatiello@users.noreply.github.com>
2021-06-03 18:25:14 +0200

    simplifying messages

--- progetto_ingsoft/src/main/java/it/polimi/ingsw/messages/GameMessage.java
+++
@@ -1,8 +0,0 @@
-package it.polimi.ingsw.messages;
-
-import it.polimi.ingsw.messages.SerializedMessage;
-
-import java.io.Serializable;
-
-public interface GameMessage extends SerializedMessage {
-}
